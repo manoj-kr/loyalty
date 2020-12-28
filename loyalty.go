d4b541e5bcfbe1f72b1065ce31098c240358a118
Manoj Kumar <manoj.kumar1@kelltontech.com>
2020-12-28 16:00:51 +0530

    loyalty model

--- loyalty.go
+++ loyalty.go
@@ -1,11 +1,23 @@
 package loyalty
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Loyalty struct {
+	Channel string         `json:"channel"`
+	Name    string         `json:"name"`
+	Meta    postgres.Jsonb `json:"meta"`
+	Data    postgres.Jsonb `json:"data"`
+}
 
 func init() {
 	fmt.Println("Init")
+	//TODO: Database tables creation
 }
 
-func Publish() {
+func (l *Loyalty) Publish() {
 	fmt.Println("Publish")
 }
